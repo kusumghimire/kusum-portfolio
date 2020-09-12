f2322f52f8aa348483ac0335853673c465c78f27
Kusum Ghimire <kusumghimire@abhiyantrik.com>
2020-09-12 18:36:38 +0545

    image css added to home.vue

--- src/main.js
+++ src/main.js
@@ -2,6 +2,8 @@ import Vue from 'vue';
 import VueRouter from 'vue-router';
 import App from './App.vue';
 import Routes from './router';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import './assets/css/style.css';
 
 Vue.use(VueRouter);
 
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,6 +1,34 @@
 <template>
-  <div class="home">
-    <h1>Kusum Ghimire</h1>
+  <div class="home container">
+    <div class="row no-gutters">
+      <div class="rol-12 col-md-6 col-lg-3 d-flex flex-column justify-content-around section-title">
+        <h1 class="text-nowrap" style="width: 8rem;">
+          Kusum
+          <br />Ghimire
+        </h1>
+        <aside class="section-about">
+          <img src="@/assets/img/coffee.png" alt="Coffee" srcset />
+          <h3 class="section-sub-title text-uppercase py-3">
+            About me
+          </h3>
+          <p>
+            Hi, I am Kusum; a frontend developer based in
+           Kathmandu, Nepal. I love working on fun little
+            projects during my free time and post educational
+           content on Instagram.
+           I am open to projects at the moment. <a href="">Contact Me</a>
+          </p>
+        </aside>
+      </div>
+      <div
+        class="rol-12 col-md-6 col-lg-5 d-flex justify-content-center align-items-end section-img"
+      >
+        <!-- <img scr="@/assets/img/coffee.png" alt="Kusum's Pic" /> -->
+          <img src="@/assets/img/profile.png" alt="Coffee" srcset />
+
+      </div>
+      <div class="rol-12 col-md-6 col-lg-4"></div>
+    </div>
   </div>
 </template>
 
@@ -11,10 +39,4 @@ export default {
 </script>
 
 <style lang="scss">
-.home {
-  background-color: #444;
-}
-h1{
-  color: #fff;
-  font-size: 40px;
-}
+</style>
