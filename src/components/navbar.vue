1f50a20a9e40c18f6a4d0a385ce109289ddbffe6
Kusum Ghimire <kusumghimire@abhiyantrik.com>
2020-09-27 14:26:58 +0545

    before added to navlist

--- src/components/navbar.vue
+++ src/components/navbar.vue
@@ -1,11 +1,13 @@
 <template>
-  <div class="navigation">
+  <div class=" navigation">
     <input type="checkbox" class="navigation__checkbox" id="navi-toggle" />
     <label for="navi-toggle" class="navigation__button">
       <span class="navigation__icon">&nbsp;</span>
     </label>
     <div class="navigation__background">&nbsp;</div>
     <nav class="navigation__nav">
+        <h1 class="title-header">Kusum
+          <br>Ghimire</h1>
       <ul class="navigation__list">
         <li class="navigation__item">
           <a href="#" class="navigation__link">Home</a>
