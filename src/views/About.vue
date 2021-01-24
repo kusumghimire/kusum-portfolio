bf16747a702f16ce1b4df5d4318d5d38e0935e71
Kusum Ghimire <kghimire@himalayantrchies.com>
2021-01-24 22:09:05 +0545

    links added to contact section

--- src/views/About.vue
+++ src/views/About.vue
@@ -14,16 +14,16 @@
       </div>
     </div>
       <div class="row">
-        <div class="col-12 col-md-12 col-lg-4 col-xl-4 d-flex flex-column  section-project">
+        <div class="col-12 col-md-12 col-lg-4 col-xl-4 d-flex flex-column  section-contact">
           <h2 class="text-nowrap">Contact Me</h2>
-          <a href=""><i class="fab fa-linkedin"></i>/kusum-ghimire</a>
-          <a href=""><i class="fab fa-instagram"></i>/codewithkusum</a>
+          <a href="https://www.linkedin.com/in/kusum-ghimire-a8bb50158/" target="_blank"><i class="fab fa-linkedin"></i> /kusum-ghimire</a>
+          <a href="https://www.instagram.com/codewithkusum/" target="_blank"><i class="fab fa-instagram"></i> /codewithkusum</a>
         </div>
-        <div class="col-12 col-md-12 col-lg-4 col-xl-4 d-flex flex-column section-project">
+        <div class="col-12 col-md-12 col-lg-4 col-xl-4 d-flex flex-column section-contact">
           <h2 class="text-nowrap">Showcases</h2>
-          <a href="">kusumghimire</a>
-          <a href="">kusumm</a>
-          <a href="">kusumghimire</a>
+          <a href=""><i class="fab fa-github"></i>/kusumghimire</a>
+          <a href=""><i class="fab fa-dribbble"></i>/kusumm</a>
+          <a href="https://codepen.io/kusumghimire"><i class="fab fa-codepen"></i>/kusumghimire</a>
         </div>
       </div>
       </div>
